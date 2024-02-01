bd0173e5e2c512879f74e4872e3b64d8bd7b9fe5
rainmonG <494460151@qq.com>
2024-02-01 23:04:27 +0800

    feat-pandas带数值横向条形图

--- test_statistics/test_pandas2.py
+++ test_statistics/test_pandas2.py
@@ -6,8 +6,9 @@
 @feature : 
 @description：
 """
-import pandas as pd
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 
 def test(chap):
@@ -41,10 +42,19 @@ def data_a(n):
     """
     df = pd.DataFrame({'a': np.arange(10), 'b': [chr(i + 50) for i in range(10)]})
     if n == 1:
+        df1 = df.append(pd.DataFrame([[np.nan, pd.NA]]))
+        print(df1['a'].nunique(dropna=False))
+        print(df1['a'].nunique())
+        df2 = df1.round({'a': 2})
+        print(df2)
+        print(df1.round(0))
+    elif n == 2:
         df1 = df[:]
-        print(df1)
-        print(df['a'].nunique())
+        df1.loc[:, 'c'] = [chr(i % 4 + 50) for i in range(10)]
+        ax = df1.groupby(by=['c'])['a'].sum().to_frame().plot(kind='barh', colormap='rainbow')
+        ax.bar_label(ax.containers[0])
+        plt.show()
 
 
 if __name__ == '__main__':
-    data_a(1)
+    data_a(2)
