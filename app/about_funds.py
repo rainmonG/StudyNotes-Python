ade281ccdc4e41aebca6c40de07470e255e23286
rainmonG <494460151@qq.com>
2024-09-20 06:05:11 +0800

    perf-funds try

--- app/about_funds.py
+++ app/about_funds.py
@@ -7,6 +7,7 @@
 @description：
 """
 import asyncio
+import datetime
 import time
 from ast import literal_eval
 import httpx
@@ -33,13 +34,33 @@ def get_all_corps() -> pd.DataFrame:
     return df_corp
 
 
-async def get_one_fund(code) -> pd.DataFrame:
+async def get_one_fund(code) -> dict:
     # 基金当前信息
     name_url = f'http://fundgz.1234567.com.cn/js/{code}.js'
     async with httpx.AsyncClient(timeout=None) as client:
         res = await client.get(name_url)
         if res.status_code != 200:
-            return pd.DataFrame()
+            return {}
+    info = res.text.strip().lstrip('jsonpgz(').rstrip(');').strip()
+    if not info:
+        return {}
+    try:
+        info = literal_eval(info)
+    except Exception:
+        print(name_url, 'bug')
+        return {}
+    print(code, info)
+    return info
+
+
+async def get_funds_info(num: int):
+    start = time.time()
+    df_funds = get_fund_names()
+    infos = []
+    for _, df_fund in df_funds.groupby(df_funds.index // num):
+        task_list = [asyncio.create_task(get_one_fund(code)) for code in df_fund['fundcode']]
+        info = await asyncio.gather(*task_list)
+        infos.extend(info)
     name_mapping = {
         'jzrq': 'start_date',  # 基金开始日期
         'dwjz': 'net_worth',  # 净值
@@ -47,27 +68,7 @@ async def get_one_fund(code) -> pd.DataFrame:
         'gszzl': 'estimate_inc',  # 估算涨幅，负值为跌
         'gztime': 'latest_time',  # 最近统计时间
     }
-    info = res.text.strip().lstrip('jsonpgz(').rstrip(');').strip()
-    if not info:
-        return pd.DataFrame()
-    try:
-        info = literal_eval(info)
-    except Exception:
-        print(name_url, 'bug')
-        return pd.DataFrame()
-    df_info = pd.Series(info).to_frame().T.rename(name_mapping)
-    print(code, info)
-    return df_info
-
-
-async def get_funds_info(num: int):
-    start = time.time()
-    df_funds = get_fund_names()
-    df_info = pd.DataFrame()
-    for _, df_fund in df_funds.groupby(df_funds.index // num):
-        task_list = [asyncio.create_task(get_one_fund(code)) for code in df_fund['fundcode']]
-        info = await asyncio.gather(*task_list)
-        df_info = pd.concat([df_info, *info])
+    df_info = pd.DataFrame(infos).rename(columns=name_mapping)
     df_funds = pd.merge(df_funds, df_info, on=['fundcode', 'name'], how='left')
     end = time.time()
     print(f'一共耗时：{end - start}')
@@ -75,4 +76,5 @@ async def get_funds_info(num: int):
 
 if __name__ == '__main__':
     df = asyncio.run(get_funds_info(8))
+    df.to_excel(r'D:\python\{}.xlsx'.format(datetime.date.today().strftime('%Y%m%d')), index=False)
     print('done')
